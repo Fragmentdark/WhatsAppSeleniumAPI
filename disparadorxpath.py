e1002320a218f879fc7cf6051fdaf2a4943e8b87
Reinaldo Silveira <fragmenthighbpm@gmail.com>
2025-04-09 18:25:13 -0300

    Add files via upload

--- disparadorxpath.py
+++ disparadorxpath.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains 
 
-# --- Função para mover o mouse 
+#Função para mover o mouse 
 def mover_mouse(driver):
     try:
         actions = ActionChains(driver)
